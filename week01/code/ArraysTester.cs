c2f6c3541067f123aa37e7210522c60aaf0e3383
Scott Burton <burtons@byui.edu>
2023-09-29 12:52:22 -0600

    Updated comments

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -1,6 +1,6 @@
 public static class ArraysTester {
     /// <summary>
-    /// Entry point for the Prove 1 tests
+    /// Entry point for the tests
     /// </summary>
     public static void Run() {
         // Sample Test Cases (may not be comprehensive)
@@ -34,7 +34,11 @@ public static class ArraysTester {
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     private static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start (don't forget to fill out the 01-prove-response.docx)
+        // TODO Problem 1 Start
+        // Remember: Using comments in your program, write down your process for solving this problem
+        // step by step before you write the code. The plan should be clear enough that it could
+        // be implemented by another person.
+
         return new double[0]; // replace this return statement with your own
     }
     
@@ -48,6 +52,10 @@ public static class ArraysTester {
     /// </summary>
     private static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start (don't forget to fill out the 01-prove-response.docx)
+        // TODO Problem 2 Start
+        // Remember: Using comments in your program, write down your process for solving this problem
+        // step by step before you write the code. The plan should be clear enough that it could
+        // be implemented by another person.
+
     }
 }
