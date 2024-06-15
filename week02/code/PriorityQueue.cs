a77ef53df3e0cb141494d27efe8a64218626cf61
Ephraim Kunz <ephraimkunz@me.com>
2024-06-15 10:39:29 -0600

    Week 2 update

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -16,8 +16,7 @@
     public String Dequeue() {
         if (_queue.Count == 0) // Verify the queue is not empty
         {
-            Console.WriteLine("The queue is empty.");
-            return null;
+            throw new InvalidOperationException("The queue is empty.");
         }
 
         // Find the index of the item with the highest priority to remove
